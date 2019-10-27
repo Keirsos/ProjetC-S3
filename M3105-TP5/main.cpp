9f9862c33d9f948f397217e0dff68659ad988aef
SimGetti <sim.getti@hotmail.fr>
2019-10-27 15:37:39 +0100

    Modif de main.cpp : normalement le prog remplit traduction.txt

--- M3105-TP5/main.cpp
+++ M3105-TP5/main.cpp
@@ -4,16 +4,22 @@ using namespace std;
 #include "Exceptions.h"
 
 int main(int argc, char* argv[]) {
-    string nomFich;
-    if (argc != 2) {
-        cout << "Usage : " << argv[0] << " nom_fichier_source" << endl << endl;
-        cout << "Entrez le nom du fichier que voulez-vous interpréter : ";
+    string nomFich, nomFichTrad;
+    if (argc < 2) {
+        cout << "Usage : " << argv[0] << " nom_fichier_source nom_fichier_traduction" << endl << endl;
+        cout << "Entrez le nom du fichier que vous voulez interpréter : ";
         getline(cin, nomFich);
-    } else
+        cout << "Entrez le nom du fichier dans lequel vous voulez écrire la traduction : ";
+        getline(cin, nomFichTrad);
+    } else{
         nomFich = argv[1];
+        nomFichTrad = argv[2];
+    }
     ifstream fichier(nomFich.c_str());
+    ofstream fichierTrad(nomFichTrad.c_str());
     try {
         if (fichier.fail()) throw FichierException();
+        if (fichierTrad.fail()) throw FichierException();
         Interpreteur interpreteur(fichier);
         interpreteur.analyse();
         // Si pas d'exception levée, l'analyse syntaxique a réussi
@@ -25,6 +31,9 @@ int main(int argc, char* argv[]) {
         if (interpreteur.getArbre() != nullptr) interpreteur.getArbre()->executer();
         // Et on vérifie qu'il a fonctionné en regardant comment il a modifié la table des symboles
         cout << endl << "================ Table des symboles apres exécution : " << interpreteur.getTable();
+        
+        //Ecriture dans le fichier de traduction
+        interpreteur.traduitEnCPP(fichierTrad,0);
     } catch (InterpreteurException & e) {
         cout << e.what() << endl;
     }
