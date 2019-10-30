3da377bdf4004cde2ca5a96f50a4a67356c618fe
Siméon Getti <sim.getti@hotmail.fr>
2019-10-30 17:55:13 +0100

    Rédaction des fichiers test

---
+++ M3105-TP5/traduction.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int var;
+
+    cout << "Hello World !" << endl;
+    var = 12;
+    cout << "var = " << var << endl;
+    cout << "Saisissez une valeur : " << endl;
+    cin >> var;
+    cout << "var = " << var << endl;
+    return 0;
+}
