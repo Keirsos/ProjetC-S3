7f55168d7310075a739d34316482e27f47a7dcf2
Flavien <Flavien@192.168.0.46>
2019-11-02 17:00:18 +0100

    deb tests unitaires

---
+++ M3105-TP5/tantQueTest.cpp
@@ -0,0 +1,79 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/*
+ * File:   tantQueTest.cpp
+ * Author: Flavien
+ *
+ * Created on 2 nov. 2019, 16:25:03
+ */
+
+#include "tantQueTest.h"
+
+//
+
+#include <iostream>
+using namespace std;
+#include "Interpreteur.h"
+#include "Exceptions.h"
+
+
+CPPUNIT_TEST_SUITE_REGISTRATION(tantQueTest);
+
+tantQueTest::tantQueTest() {
+}
+
+tantQueTest::~tantQueTest() {
+}
+
+void tantQueTest::setUp() {
+}
+
+void tantQueTest::tearDown() {
+}
+
+void tantQueTest::testMethod() {
+    CPPUNIT_ASSERT(true);
+}
+
+void tantQueTest::testFailedMethod() {
+    CPPUNIT_ASSERT(false);
+}
+
+/*****************************************************************/
+
+void tantQueTest::testSyntaxe() {
+    try{
+        
+        string nomFich = "testTantQue.txt";
+        ifstream fichier(nomFich.c_str());
+        Interpreteur interpreteur(fichier);
+        interpreteur.analyse();
+        // Si pas d'exception levée, l'analyse syntaxique a réussi
+        cout << endl << "================ Syntaxe Correcte" << endl;
+        // On affiche le contenu de la table des symboles avant d'exécuter le programme
+        cout << endl << "================ Table des symboles avant exécution : " << endl;
+        //cout << interpreteur.getTable();
+//        cout << endl << "================ Execution de l'arbre" << endl;
+//        // On exécute le programme si l'arbre n'est pas vide
+//        if (interpreteur.getArbre() != nullptr) interpreteur.getArbre()->executer();
+//        // Et on vérifie qu'il a fonctionné en regardant comment il a modifié la table des symboles
+//        cout << endl << "================ Table des symboles apres exécution : " << interpreteur.getTable();
+
+        CPPUNIT_ASSERT(true);
+    } catch (InterpreteurException & e) {
+        cout << e.what() << endl;
+        CPPUNIT_ASSERT(false);
+    }
+}
+
+void tantQueTest::testVariables() {
+    const std::string& nom = "Bonjour je suis le salarié 2";
+    if (true /*check result*/) {
+        CPPUNIT_ASSERT(false);
+    }
+}
+
---
+++ M3105-TP5/tantQueTest.h
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/*
+ * File:   tantQueTest.h
+ * Author: Flavien
+ *
+ * Created on 2 nov. 2019, 16:25:03
+ */
+
+#ifndef TANTQUETEST_H
+#define TANTQUETEST_H
+
+#include <cppunit/extensions/HelperMacros.h>
+
+class tantQueTest : public CPPUNIT_NS::TestFixture {
+    CPPUNIT_TEST_SUITE(tantQueTest);
+
+    CPPUNIT_TEST(testMethod);
+    CPPUNIT_TEST(testFailedMethod);
+    
+    //
+    
+    CPPUNIT_TEST(testSyntaxe);
+    CPPUNIT_TEST(testVariables);
+
+    CPPUNIT_TEST_SUITE_END();
+
+public:
+    tantQueTest();
+    virtual ~tantQueTest();
+    void setUp();
+    void tearDown();
+
+private:
+    void testMethod();
+    void testFailedMethod();
+    
+    //
+    
+    void testSyntaxe();
+    void testVariables();
+};
+
+#endif /* TANTQUETEST_H */
+
